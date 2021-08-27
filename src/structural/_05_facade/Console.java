33018ff9597684c68d0aab4de542e36b502afee6
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2021-08-27 18:04:09 +0530

    facade pattern

---
+++ src/structural/_05_facade/Console.java
@@ -0,0 +1,37 @@
+package structural._05_facade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Console {
+    private List<Viewport> viewports = new ArrayList<>();
+    int width, height;
+
+    public Console(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void addViewport(Viewport viewport) {
+        viewports.add(viewport);
+    }
+
+    public void render() {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                for (Viewport vp : viewports) {
+                    System.out.println(vp.charAt(x, y));
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static Console newConsole(int width, int height) {
+        Buffer buffer = new Buffer(height, width);
+        Viewport viewport = new Viewport(buffer, 30, height, 0, 0);
+        Console console = new Console(width, height);
+        console.addViewport(viewport);
+        return console;
+    }
+}
---
+++ src/structural/_05_facade/facade.md
@@ -0,0 +1,10 @@
+## FACADE PATTERN
+
+### FACADE :
+    Provides a simple, easy to understand user interface over a large and sophisticated body of code.
+
+### SUMMARY
+
+- Build a Facade to provide a simplified API over a set of classes. 
+- May wish to optionally expose internals through the facade.
+- May allow users to escalate to use more complex APIs if they need to.
